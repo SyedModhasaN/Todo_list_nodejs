75d3d267b7ac121e06833089304efcc8555c38eb
Syed Modassir Hasan <modassir77@gmail.com>
2025-09-20 23:23:54 +0530

    mongo warning removed

--- app.js
+++ app.js
@@ -7,10 +7,11 @@ var app = express();
 app.set("view engine","ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({extended:true}));
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+ mongoose.connect(process.env.MONGO_URI)
+//  , {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 // var items = [];
